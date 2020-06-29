d74714dbc1abf44465aaf5f51c4c6a0722a68ae5
Juan Oscco Mori <juan.osccomori@gmail.com>
2020-06-28 23:28:13 -0500

    se agregaron tres rutas API

--- app/cine/urls.py
+++ app/cine/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Home, ListPelicula, RegistroCliente
+from .views import Home, ListFuncion, RegistroCliente, ListPeliculas
 
 urlpatterns = [
     path('', Home.as_view(), name='index'),
-    path('registro_cliente', RegistroCliente.as_view(), name='registro.cliente' ),
-    path('list_pelicula', ListPelicula.as_view(), name='list.pelicula')
+    path('list_funcion/<int:pk>', ListFuncion.as_view(), name='list.funcion'),
+    path('list_pelicula', ListPeliculas.as_view(), name='list.pelicula'),
+    path('registro_cliente', RegistroCliente.as_view(), name='registro.cliente' )
 ]
--- app/cine/views.py
+++ app/cine/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, CreateView
-from .models import Pelicula, Cliente
+from .models import Pelicula, Cliente, Funcion
 from .forms import RegistroCliente
 from django.urls import reverse_lazy
 
@@ -8,14 +8,20 @@ from django.urls import reverse_lazy
 class Home(TemplateView):
     template_name = 'cine/index.html'
 
-class ListPelicula(ListView):
-    model = Pelicula
-    template_name = 'cine/pelicula/list_pelicula.html'
-    queryset = Pelicula.objects.all()
-    context_object_name = 'peliculas'
-
 class RegistroCliente(CreateView):
     model = Cliente
     form_class = RegistroCliente
     template_name = 'cine/cliente/registro_cliente.html'
     success_url = reverse_lazy('cine:list.pelicula')
+
+class ListFuncion(ListView):
+    model = Funcion
+    template_name = 'cine/pelicula/list_funcion.html'
+    queryset = Funcion.objects.all()
+    context_object_name = 'funciones'
+
+class ListPeliculas(ListView):
+    model = Pelicula
+    template_name = 'cine/pelicula/list_pelicula.html'
+    queryset = Pelicula.objects.all()
+    context_object_name = 'peliculas'
